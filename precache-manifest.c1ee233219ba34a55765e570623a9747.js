76ff02d1289c4284ddd63fbfe03f2b686f6d409f
Dhanuvel163 <dhanuram99@gmail.com>
2020-08-03 19:13:51 +0530

    Updates

---
+++ precache-manifest.c1ee233219ba34a55765e570623a9747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d128d7b44beef6dfd04f31c03f8cedd0",
+    "url": "/Notemaking-React/index.html"
+  },
+  {
+    "revision": "1f3cb685a6166a6b999b",
+    "url": "/Notemaking-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0cf251dd8eb9dcc5c83",
+    "url": "/Notemaking-React/static/css/main.e6c0d60a.chunk.css"
+  },
+  {
+    "revision": "1f3cb685a6166a6b999b",
+    "url": "/Notemaking-React/static/js/2.6b9d4bcc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Notemaking-React/static/js/2.6b9d4bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cf251dd8eb9dcc5c83",
+    "url": "/Notemaking-React/static/js/main.9825d8ba.chunk.js"
+  },
+  {
+    "revision": "fc47ea253bb0822aa1ec",
+    "url": "/Notemaking-React/static/js/runtime-main.f45b0387.js"
+  }
+]);
